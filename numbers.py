f7ed2a83e65d334f88b287abf014bf6ba8319409
Thalita <thalitamunique@gmail.com>
2024-01-11 14:08:18 +0900

    Leap Year

--- numbers.py
+++ numbers.py
@@ -16,6 +16,7 @@ print(rev)
 print(rev[::-1])
 '''
 ####Fibonatti
+'''
 a=0
 b=1
 limit=int(input("Enter the limit of numbers:"))-2
@@ -24,3 +25,26 @@ for i in range (limit):
     a=b
     b=c
 print(b)
+'''
+### Armstrong Number
+'''
+number1= input("Write a number to be verified:")
+number2=0
+factor = len(number1)
+print (factor)
+for i in range (factor):
+    number2= number2 + int(number1[i]) ** factor
+if number2 == int(number1):
+    print("Armstrong!")
+else:
+    print("Not Armstrong!")
+ ''' 
+  ###Leap Year
+def leapyear(ano):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        result = " is a Leap Year"
+    else:
+        result = " is not a Leap Year"
+    return result
+year = int(input("Type the year:"))
+print(str(year) + str(leapyear(year)) )
