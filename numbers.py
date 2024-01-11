d71b7e6cc65fe7926517639ff9c172d938806f81
Thalita <thalitamunique@gmail.com>
2024-01-11 13:47:38 +0900

    Fibonatti

--- numbers.py
+++ numbers.py
@@ -1,5 +1,5 @@
 #########Decimal to binary
-
+'''
 num=int(input("Enter an integer number: "))
 print(bin(num)[2:])
 
@@ -14,3 +14,13 @@ def binary(num):
 rev=binary(num)
 print(rev)
 print(rev[::-1])
+'''
+####Fibonatti
+a=0
+b=1
+limit=int(input("Enter the limit of numbers:"))-2
+for i in range (limit):
+    c=a+b
+    a=b
+    b=c
+print(b)
